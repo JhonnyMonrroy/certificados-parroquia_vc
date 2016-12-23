2a414f920e1a98cbcaf500b2e504941ae009fd0a
Jhonny Monrroy <jhonnymonrroy@yahoo.es>
2016-12-23 01:18:49 -0400

    Base para spring

---
+++ src/main/java/com/breakdark/certificados_parroquia_vc/App.java
@@ -0,0 +1,13 @@
+package com.breakdark.certificados_parroquia_vc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/breakdark/certificados_parroquia_vc/model/Bautismo/BautismoJDBCTemplate.java
@@ -0,0 +1,5 @@
+package com.breakdark.certificados_parroquia_vc.model.Bautismo;
+
+public class BautismoJDBCTemplate {
+
+}
