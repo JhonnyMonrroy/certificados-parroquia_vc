2a53b5159b31bc50e815b65b710453a11a7dea50
Jhonny Monrroy <jhonnymonrroy@yahoo.es>
2016-12-25 14:31:34 -0400

    base para las pruebas de unicidad

---
+++ src/test/java/com/breakdark/certificados_parroquia_vc/model/Bautismo/BautismoServiceTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.breakdark.certificados_parroquia_vc.model.Bautismo;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import junit.framework.TestCase;
+
+/**
+ * @author BreakDark
+ * @version 1.0
+ */
+public class BautismoServiceTest extends TestCase {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		// ApplicationContext context = new
+		// ClassPathXmlApplicationContext("spring-config.xml");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.breakdark.certificados_parroquia_vc.model.Bautismo.BautismoService#adicionarBautismo(com.breakdark.certificados_parroquia_vc.model.Bautismo.Bautismo)}.
+	 */
+	@Test
+	public void testAdicionarBautismo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.breakdark.certificados_parroquia_vc.model.Bautismo.BautismoService#obtenerTodosLosBautismos()}.
+	 */
+	@Test
+	public void testObtenerTodosLosBautismos() {
+		fail("Not yet implemented");
+	}
+
+}
